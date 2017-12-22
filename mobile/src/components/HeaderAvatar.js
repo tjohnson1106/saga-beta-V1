a446279227f76460368ec5434c0a8240c62bde2a
Thomas Johnson <tjohnson1106@gmail.com>
2017-12-22 15:23:56 -0500

    added plus.png icon to upper right hand of home screen(this may change based on decsion to use tab-bar-ios)icon is touchable

--- mobile/src/components/HeaderAvatar.js
+++ mobile/src/components/HeaderAvatar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import styled from "styled-components/native";
-import Touchable from "@appandflow/touchable";
 import { connect } from "react-redux";
 import { connectActionSheet } from "@expo/react-native-action-sheet";
 import { withApollo } from "react-apollo";
@@ -18,19 +17,6 @@ height: ${AVATAR_SIZE};
 width: ${AVATAR_SIZE};
 /* prettier-ignore */
 borderRadius: ${AVATAR_RADIUS};
-`;
-
-const Button = styled(Touchable).attrs({
-  feedback: "opacity",
-  /* prettier-ignore */
-  hitSlop: { top: 20, bottom: 20, right: 20, left: 20 }
-})`
-  /* prettier-ignore */
-  marginLeft: 15;
-  /* prettier-ignore */
-  justifyContent: center;
-  /* prettier-ignore */
-  alignItems: center;
 `;
 
 class HeaderAvatar extends Component {
