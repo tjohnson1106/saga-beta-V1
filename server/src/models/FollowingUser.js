14734b17dd7aaf11f9fe2efa802b0bf1b33ceac0
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-01-03 16:43:22 -0500

    created userId and followings field

--- server/src/models/FollowingUser.js
+++ server/src/models/FollowingUser.js
@@ -1,7 +1,17 @@
-// import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-// const FollowingUserSchema = new Schema ({
+const FollowingUserSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    unique: true
+  },
+  followings: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
 
-// });
+FollowingUserSchema.index({ userId: 1 }, { unique: true });
 
-// export default mongoose.model("FollowingUser", FollowingUserSchema);
+export default mongoose.model("FollowingUser", FollowingUserSchema);
